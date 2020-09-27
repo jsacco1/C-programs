57bbc5aaab928bbb11a785690b26dca8dbfc2d9e
James Sacco <jsacco001@gmail.com>
2020-09-27 13:28:54 -0700

    spaces

--- strings_intro.cpp
+++ strings_intro.cpp
@@ -8,13 +8,16 @@ using namespace std;
 int main() {
     // Provided two strings a and b:
     string a, b;
+    
     // Input a and b.
     cin >> a >> b;
+    
     // Print lengths of a and b.
     // Print concatenation (a + b)
     // Print a and b with their first letters swapped.
     cout << a.size() << " " << b.size() << "\n" << a + b << "\n";
-    cout << b[0] + a.substr(1) << " " << a[0] + b.substr(1);    
+    cout << b[0] + a.substr(1) << " " << a[0] + b.substr(1);  
+    
     return 0;
 
 }
