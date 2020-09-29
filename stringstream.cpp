5ad000fd394f949b8dbcf144d4cf5dc0247480c9
James Sacco <jsacco001@gmail.com>
2020-09-28 23:39:11 -0700

    stringstream

---
+++ stringstream.cpp
@@ -0,0 +1,36 @@
+// StringStream
+// James Sacco
+
+// Description:
+// input one line of n integers separated by commas.
+// returns a vector of parsed integers
+
+#include <sstream>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> parseInts(string str) {
+	// create stringstream
+    stringstream ss(str);
+    int num;
+    char ch;
+    vector<int> res;
+    while(ss >> num){
+        ss >> ch;
+        res.push_back(num);
+    }
+    return res;
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << "\n";
+    }
+    
+    return 0;
+}
+ 
